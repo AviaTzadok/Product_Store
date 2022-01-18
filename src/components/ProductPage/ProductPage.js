f9d401a2b308cb43e45d269a081343a6e93a43d5
AviaTzadok <73134926+AviaTzadok@users.noreply.github.com>
2022-01-18 17:20:29 +0200

    commit add page product

--- src/components/ProductPage/ProductPage.js
+++ src/components/ProductPage/ProductPage.js
@@ -1,8 +1,38 @@
 import "./ProductPage.css";
 import { Link, useParams } from "react-router-dom";
-import React from "react";
+import { useState, useEffect, useRef } from "react";
+import Product from "../Product/Product";
 
 export default function ProductPage() {
+  const [productPage, setProductPage] = useState([]);
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products/")
+      .then((res) => res.json())
+      .then((products) => {
+        setProductPage(products);
+      });
+  }, []);
+  console.log(productPage);
   let { id } = useParams();
-  return <Link to={`/`}>{id}</Link>;
+  console.log(id);
+  let product = productPage.find((v) => v.id == id);
+  console.log(product);
+  if (product == undefined) {
+    return <div></div>;
+  }
+  return (
+    <div>
+      <Product
+        key={product.id}
+        id={product.id}
+        title={product.title}
+        price={product.price}
+        description={product.description}
+        category={product.category}
+        image={product.image}
+        rate={product.rating?.rate}
+        count={product.rating?.count}
+      />
+    </div>
+  );
 }
